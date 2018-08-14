1ce803b1b00ef3e9d868dfddccd1c819124f13f2
Mike Splain <mike.splain@gmail.com>
2018-08-14 11:41:52 -0400

    Fix regex test

--- script/test-install.sh
+++ script/test-install.sh
@@ -4,8 +4,8 @@ sudo spctl --master-disable
 brew update
 
 # Remove casks and mas since they're big
-sed -i '' 's/.*cask.*//g' Brewfile
-sed -i '' 's/.*mas.*//g' Brewfile
+sed -i '' 's/^cask.*//g' Brewfile
+sed -i '' 's/^mas.*//g' Brewfile
 
 # Remove iftop since it gives a sudo error
 sed -i '' 's/.*iftop.*//g' Brewfile
