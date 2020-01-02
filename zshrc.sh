d7699cca5c0faa9dd5b478db9482708832423efc
mikesplain <mike.splain@gmail.com>
2020-01-02 15:23:13 -0500

    Remove brew plugin

--- zshrc.sh
+++ zshrc.sh
@@ -56,7 +56,7 @@ ZSH_THEME="amuse"
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(git brew docker zsh-autosuggestions)
+plugins=(git docker zsh-autosuggestions)
 
 source $ZSH/oh-my-zsh.sh
 
