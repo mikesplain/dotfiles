4f89605fb7cb5255875a9856548a564b4c3a4d73
Mike Splain <mike.splain@gmail.com>
2022-02-11 14:04:11 -0500

    Sub

--- run_after_install-asdf.sh
+++ run_after_install-asdf.sh
@@ -4,7 +4,7 @@
 
 echo "Installing asdf plugins"
 
-cut -d' ' -f1 $HOME/.tool-versions | xargs -I {} asdf plugin add {}
+cut -d' ' -f1 ${HOME}/.tool-versions | xargs -I {} asdf plugin add {}
 
 echo "Installing asdf tools"
 
