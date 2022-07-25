f5d41399ce99ee3442a1954dc2cda9063ecc8529
mikesplain <mike.splain@gmail.com>
2022-07-25 12:09:28 -0400

    Remove debug

--- install.sh
+++ install.sh
@@ -25,5 +25,4 @@ fi
 # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
 script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
 # exec: replace current process with chezmoi init
-echo "$chezmoi" init --apply "--source=$script_dir" "$verbose"
 exec "$chezmoi" init --apply "--source=$script_dir" "$verbose"
