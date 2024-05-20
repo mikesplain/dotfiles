0a2d7acc580b9abc9ae5507988cab0a8f2addd41
mikesplain <mike.splain@gmail.com>
2024-05-20 14:54:02 -0400

    Try abstract configs

--- nixos-config/modules/global/macos.nix
+++ nixos-config/modules/global/macos.nix
@@ -4,8 +4,24 @@
   pkgs,
   options,
   pwnvim,
+  homebrew-core,
+  homebrew-cask,
+  homebrew-bundle,
   ...
-}: {
+}: let
+  nix-homebrew = {
+      # inherit user;
+      enable = true;
+      user = config.cfg.user.name;
+      taps = {
+        "homebrew/homebrew-core" = homebrew-core;
+        "homebrew/homebrew-cask" = homebrew-cask;
+        "homebrew/homebrew-bundle" = homebrew-bundle;
+      };
+      mutableTaps = false;
+      autoMigrate = true;
+  };
+  in {
   cfg.os.name = "macos";
   system.stateVersion = 4;
 
