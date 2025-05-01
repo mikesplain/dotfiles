69352c437aabc73ad90376c5629b3831254dcb9d
Mike Splain <mike.splain@gmail.com>
2025-05-01 09:44:36 -0400

    Fix cmd l

--- home/shell.nix
+++ home/shell.nix
@@ -73,7 +73,17 @@
       # Cisco Specific Stuff
       [[ -f $HOME/.zshrc-cisco ]] && source $HOME/.zshrc-cisco
 
-
+      # Clear the screen and scrollback buffer
+      function clear-screen-and-scrollback() {
+        builtin echoti civis >"$TTY"
+        builtin print -rn -- $'\e[H\e[2J' >"$TTY"
+        builtin zle .reset-prompt
+        builtin zle -R
+        builtin print -rn -- $'\e[3J' >"$TTY"
+        builtin echoti cnorm >"$TTY"
+      }
+      zle -N clear-screen-and-scrollback
+      bindkey '^L' clear-screen-and-scrollback
 
     '';
   };
