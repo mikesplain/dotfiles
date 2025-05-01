8da99a981ae29418054c2cf232955f628a63d9b5
Mike Splain <mike.splain@gmail.com>
2025-05-01 10:08:09 -0400

    Fix mise script

--- home/shell.nix
+++ home/shell.nix
@@ -63,7 +63,7 @@
 
       export SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
 
-      eval "$(mise activate zsh)"'
+      eval "$(mise activate zsh)"
 
       # Switch to the current flake
       switch() {
