eb058353add116e2e96d8e112433522338ff4b73
Mike Splain <mike.splain@gmail.com>
2025-06-02 15:43:09 -0400

    Updates

--- darwin/default.nix
+++ darwin/default.nix
@@ -8,6 +8,9 @@
   '';
 
   security.pam.services.sudo_local.touchIdAuth = true;
+  security.pam.services.sudo_local.watchIdAuth = true;
+  security.pam.services.sudo_local.reattach = true;
+
   fonts.packages = [ pkgs.nerd-fonts.meslo-lg ];
 
   nix = {
