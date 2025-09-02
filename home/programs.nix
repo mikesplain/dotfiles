dd694f550577c79538eb28f93e9bab5e89405693
Mike Splain <mike.splain@gmail.com>
2025-09-02 09:21:21 -0400

    Fix quote issue

--- home/programs.nix
+++ home/programs.nix
@@ -64,7 +64,7 @@
       # Default configuration for all hosts
       "*" = {
         extraOptions = {
-          IdentityAgent = "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
+          IdentityAgent = "\"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock\"";
           StrictHostKeyChecking = "ask";
           VerifyHostKeyDNS = "ask";
           NoHostAuthenticationForLocalhost = "yes";
