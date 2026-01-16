5c2a9e062ec0386fa52e01f29a5be80c7fada0bd
Mike Splain <mike.splain@gmail.com>
2026-01-16 11:27:48 -0500

    Add --yolo to codex alias

--- home/shell.nix
+++ home/shell.nix
@@ -42,7 +42,7 @@
       update_flake_from_pr = "__flake_update_merge";
       w = "windsurf .";
       c = "cursor .";
-      codex = "GITHUB_PAT_TOKEN=$(gh auth token) command codex";
+      codex = "GITHUB_PAT_TOKEN=$(gh auth token) command codex --yolo";
     };
 
     sessionVariables = {
